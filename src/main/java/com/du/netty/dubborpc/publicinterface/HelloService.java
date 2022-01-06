9f6f1fe80bc5a2993be5786b4812fc496bf0106b
dzb <dzbcrfx@outlook.com>
2022-01-06 22:31:28 +0800

    备份

---
+++ src/main/java/com/du/netty/dubborpc/publicinterface/HelloService.java
@@ -0,0 +1,10 @@
+package com.du.netty.dubborpc.publicinterface;
+
+/**
+ * @author: duzhibin
+ * @description:
+ * @date: create in 22:58 2021/12/31
+ */
+public interface HelloService {
+    String hello(String msg);
+}
